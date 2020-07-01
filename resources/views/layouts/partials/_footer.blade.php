6c5c15aed92022ac9cf06ff7c782592965affcb3
KONE YACOUBA <fellinikone@gmail.com>
2020-07-01 23:39:54 +0000

    Setup and add static pages

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center">
+	<p>&copy; {{date('y')}} &middot;Laracarte by <a href="https://twitter.com/WLeylu"> @WLeylu</a> - A Laramap clone app.</p>
+
+	<p><b>This app has been built for learning purposes.</b></p>
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+
+@section('content')
+        <h1 class="text-center">La carte ici !!!!!!!!!!!!!!!</h1>
+
+@stop
+
+
